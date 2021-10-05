f30a6a2614b238faa9a4438c133cb924fbd68757
ayeletDayan <91838020+ayeletDayan@users.noreply.github.com>
2021-10-05 19:03:14 +0300

    3rd version

--- mineSweeper.js
+++ mineSweeper.js
@@ -1,18 +1,29 @@
 'use strict'
 var gSize
 var gMines
+var gBoard = []
 var gTimeInterval
 var gTime
 var gLife = 3
+var gHint = 3
 var gOpenCells = 0
 var gFlagCount = 0
+var isHint
+var gHintInterval
+var bestScore = {
+    beginner: 0,
+    medium: 0,
+    expert: 0
+}
+var gSafeClick = 3
+var gUndoArr = []
 
 const MINE = '&#128163'
 const EMPTY = '&#127808'
 const FLAG = '&#128681'
 const LIFE = '&#128151'
+const HINT = '&#10067'
 
-var gBoard = []
 
 var gGame = {
     isOn: false,
@@ -28,11 +39,16 @@ function initGame() { //This is called when page loads
     var elTime = document.querySelector('span')
     elTime.innerText = '00:00'
     gLife = 3
+    gHint = 3
+    gSafeClick = 3
     gOpenCells = 0
     gFlagCount = 0
+    var gUndoArr = []
     buildBoard()
     renderBoard(gBoard)
-    renderLife()
+    renderObject(gLife, LIFE, '.life')
+    renderObject(gHint, HINT, '.hint')
+    document.querySelector('.safe-Click').innerHTML = '3 Safe Click'
 }
 
 function start() {
@@ -40,20 +56,31 @@ function start() {
 }
 
 function cellClicked(elCell, i, j, ev) { //Called when a cell (td) is clicked
+    //hint    
+    if (isHint) showHint(i, j)
+    //game
+    if (!gGame.isOn && ev !== 3 && gBoard[i][j].in === MINE) return
     if (gBoard[i][j].isOpen) return
-    if (!gGame.isOn) {
-        if (gBoard[i][j].in === MINE) return
-        gTime = Date.now()
-        gTimeInterval = setInterval(stopper, 10)
+    if (!gGame.isOn && ev === 3) {
+        startTime()
+        gGame.isOn = true
+        gBoard[i][j].out = FLAG
+        gUndoArr.push({i: i,j: j, in: gBoard[i][j].in, out:  gBoard[i][j].out})        
+        gFlagCount++
+    }
+    if (!gGame.isOn && gBoard[i][j].in !== MINE) {
+        startTime()
         gGame.isOn = true
     }
     if (ev === 3) {
         if (gBoard[i][j].out === FLAG) {
             gBoard[i][j].out = ''
+            gUndoArr.push({i: i,j: j, in: gBoard[i][j].in, out:  gBoard[i][j].out})
             gFlagCount--
         }
         else {
             gBoard[i][j].out = FLAG
+            gUndoArr.push({i: i,j: j, in: gBoard[i][j].in, out:  gBoard[i][j].out})
             gFlagCount++
         }
         renderBoard(gBoard)
@@ -61,21 +88,22 @@ function cellClicked(elCell, i, j, ev) { //Called when a cell (td) is clicked
     else if (gBoard[i][j].out === FLAG) return
     else if (gBoard[i][j].in === EMPTY) {
         gBoard[i][j].out = EMPTY
-        gOpenCells++
+        gUndoArr.push({i: i,j: j, in: gBoard[i][j].in, out:  gBoard[i][j].out})
         renderBoard(gBoard)
         openEmptyNeighbors(i, j)
     }
     else {
         gBoard[i][j].out = gBoard[i][j].in
+        gUndoArr.push({i: i,j: j, in: gBoard[i][j].in, out:  gBoard[i][j].out})
         gBoard[i][j].isOpen = true
         renderBoard(gBoard)
         gOpenCells++
-        if (gBoard[i][j].out === MINE){
+        if (gBoard[i][j].out === MINE) {
             gLife--
-            renderLife()
-        }            
+            renderObject(gLife, LIFE, '.life')
+        }
     }
-    checkGameOver()
+    checkGameOver()    
 }
 
 function checkGameOver() { //Game ends when all mines are marked, and all the other cells are shown
@@ -95,11 +123,31 @@ function checkGameOver() { //Game ends when all mines are marked, and all the ot
         renderStart(true)
         console.log('You Win!')
         clearInterval(gTimeInterval)
+        var score = Date.now() - gTime
+        checkBestScore(score)
     }
 }
 
-function expandShown(board, elCell,
-    i, j) {
-    //When user clicks a cell with no mines around, we need to open not only that cell, but also its neighbors. NOTE: start with a basic implementation that only opens the non-mine 1st degree neighbors BONUS: if you have the time later, try to work more like the real algorithm (see description at the Bonuses section below)
+function checkBestScore(newScore) {
+    switch (gSize) {
+        case 4:
+            if (!bestScore.beginner || newScore < bestScore.beginner) {
+                bestScore.beginner = newScore
+                localStorage.beginnerBestScore = newScore
+            }
+            break
+        case 8:
+            if (!bestScore.medium || newScore < bestScore.medium) {
+                bestScore.medium = newScore
+                localStorage.mediumBestScore = newScore
+            }
+            break
+        case 12:
+            if (!bestScore.expert || newScore < bestScore.expert) {
+                bestScore.expert = newScore
+                localStorage.expertBestScore = newScore
+            }
 
+            break
+    }
 }
--- utils_mineSweeper.js
+++ utils_mineSweeper.js
@@ -3,6 +3,9 @@ window.addEventListener("contextmenu", e => e.preventDefault());
 function level(size, mines) {
   gSize = size
   gMines = mines
+  var score = (size === 4) ? localStorage.beginnerBestScore : (size === 8) ? localStorage.mediumBestScore : localStorage.expertBestScore
+  var elBestScore = document.querySelector('.bestScore')
+  elBestScore.innerHTML = score / 1000
 }
 
 function buildBoard() { //Builds the board Set mines at random locations Call setMinesNegsCount() Return the created boar
@@ -20,9 +23,9 @@ function buildBoard() { //Builds the board Set mines at random locations Call se
     var idxI = getRandomInt(0, gSize - 1)
     var idxJ = getRandomInt(0, gSize - 1)
     if (gBoard[idxI][idxJ].in === MINE) i--
-    else gBoard[idxI][idxJ].in = MINE      
+    else gBoard[idxI][idxJ].in = MINE
   }
-  
+
   for (var i = 0; i < gSize; i++) {
     for (var j = 0; j < gSize; j++) {
       if (gBoard[i][j].in !== MINE) {
@@ -46,9 +49,15 @@ function renderBoard(board) { //Render the board as a <table> to the page
   elBoard.innerHTML = strHTML
 }
 
+function startTime() {
+  gTime = Date.now()
+  gTimeInterval = setInterval(stopper, 100)
+  gGame.isOn = true
+}
+
 function stopper() {
   var now = (Date.now() - gTime) / 1000
-  var elTime = document.querySelector('span')
+  var elTime = document.querySelector('.time')
   elTime.innerText = now
 }
 
@@ -69,12 +78,12 @@ function checkNeighbors(board, colIdx, rowIdx, MaxRowLength, MaxColLength) {
   return count
 }
 
-function renderLife() {
+function renderObject(sum, object, value) {
   var strHTML = ''
-  for (var i = 0; i < gLife; i++) {
-    strHTML += LIFE
+  for (var i = 0; i < sum; i++) {
+    strHTML += object
   }
-  var elLife = document.querySelector('.life')
+  var elLife = document.querySelector(value)
   elLife.innerHTML = strHTML
 }
 
@@ -100,10 +109,173 @@ function openEmptyNeighbors(i, j) {
       if (!gBoard[k][l].isOpen && gBoard[k][l].in === EMPTY) {
         gBoard[k][l].out = EMPTY
         gBoard[k][l].isOpen = true
-        openEmptyNeighbors(k, l)
         gOpenCells++
+        openEmptyNeighbors(k, l)
       }
     }
   }
   renderBoard(gBoard)
 }
+
+function hint() {
+  if (gHint === 0) return
+  var elHint = document.querySelector('.hint')
+  elHint.innerHTML = 'Hint'
+  isHint = true
+}
+
+function showHint(colIdx, rowIdx) {
+  var hintCells = []
+  for (var i = colIdx - 1; i <= colIdx + 1; i++) {
+    if (i < 0 || i > gSize - 1) continue // not outside board
+    for (var j = rowIdx - 1; j <= rowIdx + 1; j++) {
+      if (j < 0 || j > gSize - 1) continue // not outside board
+      if (gBoard[i][j].out === '') {
+        gBoard[i][j].out = gBoard[i][j].in
+        hintCells.push({ i, j })
+      }
+    }
+  }
+  isHint = false
+  gHint--
+  renderObject(gHint, HINT, '.hint')
+  renderBoard(gBoard)
+  setTimeout(function () {
+    hideHint(hintCells)
+  }, 1000);
+}
+
+function hideHint(cells) {
+  for (var i = 0; i < cells.length; i++) {
+    gBoard[cells[i].i][cells[i].j].out = ''
+    gBoard[cells[i].i][cells[i].j].isOpen = false
+    renderBoard(gBoard)
+  }
+}
+
+function safeClick() {
+  if (gSafeClick === 0) return
+  var idxI = getRandomInt(0, gSize - 1)
+  var idxJ = getRandomInt(0, gSize - 1)
+  if (!gBoard[idxI][idxJ].isOpen && gBoard[idxI][idxJ].in !== MINE) {
+    gBoard[idxI][idxJ].out = gBoard[idxI][idxJ].in
+    renderBoard(gBoard)
+  }
+  else safeClick()
+
+  gSafeClick--
+
+  var strHTML = ''
+  strHTML += (gSafeClick === 2) ? '2 Safe Click' : (gSafeClick === 1) ? '1 Safe Click' : 'No Safe Click'
+  var elsafeClick = document.querySelector('.safe-Click')
+  elsafeClick.innerHTML = strHTML
+
+  setTimeout(function () {
+    gBoard[idxI][idxJ].out = ''
+    renderBoard(gBoard)
+  }, 1000);
+
+}
+
+function undo() {
+  // if game over and all the mines are showen, so it can cover then again but i think we should not alow it... i didn't finish all the condition for this issue
+  if ((gSize === 4 && gLife === 1) || (gLife === 0)) {
+    for (var i = 0; i < gSize; i++) {
+      for (var j = 0; j < gSize; j++) {
+        if (gBoard[i][j].in === MINE) gBoard[i][j].out = ''
+      }
+    }
+  }
+    console.log('undo')
+    console.log(gUndoArr)
+    gBoard[gUndoArr[gUndoArr.length - 1].i][gUndoArr[gUndoArr.length - 1].j].out = ''
+    gBoard[gUndoArr[gUndoArr.length - 1].i][gUndoArr[gUndoArr.length - 1].j].isOpen = false
+    closeEmptyNeighbors(gUndoArr[gUndoArr.length - 1].i, gUndoArr[gUndoArr.length - 1].j)
+    renderBoard(gBoard)
+    gUndoArr.pop(1)
+    gOpenCells--
+  }
+
+  function closeEmptyNeighbors(i, j) {
+    for (var k = i - 1; k <= i + 1; k++) {
+      if (k < 0 || k > gSize - 1) continue // not outside board
+      for (var l = j - 1; l <= j + 1; l++) {
+        if (l < 0 || l > gSize - 1) continue // not outside board           
+        if (k === i && l === j) continue // not on currCel
+        if (gBoard[k][l].out === EMPTY) {
+          gBoard[k][l].out = ''
+          gBoard[k][l].isOpen = false
+          gOpenCells--
+          closeEmptyNeighbors(k, l)
+        }
+      }
+    }
+    renderBoard(gBoard)
+  }
+
+  function sevenBoom() {
+    for (var i = 0; i < gSize; i++) {
+      gBoard[i] = []
+      for (var j = 0; j < gSize; j++) {
+        gBoard[i][j] = {
+          in: EMPTY,
+          out: '',
+          isOpen: false
+        }
+      }
+    }
+
+    switch (gSize) {
+      case 4: {
+        gBoard[1][2].in = MINE
+        gBoard[3][1].in = MINE
+        break
+      }
+      case 8:
+        {
+          gBoard[0][6].in = MINE
+          gBoard[1][5].in = MINE
+          gBoard[2][4].in = MINE
+          gBoard[3][3].in = MINE
+          gBoard[4][2].in = MINE
+          gBoard[5][1].in = MINE
+          gBoard[6][0].in = MINE
+          gBoard[6][7].in = MINE
+          gBoard[7][6].in = MINE
+          break
+        }
+      case 12:
+        gBoard[0][6].in = MINE
+        gBoard[1][1].in = MINE
+        gBoard[1][8].in = MINE
+        gBoard[2][3].in = MINE
+        gBoard[2][10].in = MINE
+        gBoard[3][5].in = MINE
+        gBoard[4][0].in = MINE
+        gBoard[4][7].in = MINE
+        gBoard[5][9].in = MINE
+        gBoard[5][2].in = MINE
+        gBoard[6][4].in = MINE
+        gBoard[6][11].in = MINE
+        gBoard[7][6].in = MINE
+        gBoard[8][1].in = MINE
+        gBoard[8][8].in = MINE
+        gBoard[9][3].in = MINE
+        gBoard[9][10].in = MINE
+        gBoard[10][5].in = MINE
+        gBoard[11][0].in = MINE
+        gBoard[11][7].in = MINE
+        break
+    }
+
+    for (var i = 0; i < gSize; i++) {
+      for (var j = 0; j < gSize; j++) {
+        if (gBoard[i][j].in !== MINE) {
+          var neighbors = checkNeighbors(gBoard, i, j, gSize, gSize)
+          if (neighbors) gBoard[i][j].in = neighbors
+        }
+      }
+    }
+
+    renderBoard(gBoard)
+  }
